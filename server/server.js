889e6473bb8f20228f83b39ee3a43545b72f3108
AvishkarMandlik <mandlikavi121@gmail.com>
2025-03-28 03:40:20 +0530

    update server.js: enhance room joining logic for chat and video, add new features including screen sharing and reactions

--- server/server.js
+++ server/server.js
@@ -43,142 +43,115 @@ const onlineUsers = {}; // Tracks users in each room
 io.on('connection', (socket) => {
   console.log('New user connected:', socket.id);
 
-  socket.on('joinRoom', async ({ username, room }) => {
-      socket.join(room);
+  // Combined room joining for both chat and video
+  socket.on('joinRoom', async ({ username, room, isVideoRoom = false }) => {
+    socket.join(room);
 
-      const messages = await Message.find({ room }).sort('createdAt');
-      socket.emit('chatHistory', messages);
+    if (!onlineUsers[room]) {
+      onlineUsers[room] = [];
+    }
+
+    // Add user to room
+    onlineUsers[room].push({ userId: socket.id, username });
+
+    // Send chat history
+    const messages = await Message.find({ room }).sort('createdAt');
+    socket.emit('chatHistory', messages);
+
+    // Different events for video vs regular chat
+    if (isVideoRoom) {
+      // Video room specific events
+      socket.broadcast.to(room).emit('newUser', { userId: socket.id, username });
+      io.to(room).emit('participantCount', { count: onlineUsers[room].length });
+    } else {
+      // Regular chat events
       socket.broadcast.to(room).emit('userJoined', { id: socket.id, username });
+    }
 
-      onlineUsers[room] = [...(onlineUsers[room] || []), username];
-      io.to(room).emit('onlineUsers', onlineUsers[room]);
+    // Update online users for both cases
+    io.to(room).emit('onlineUsers', onlineUsers[room]);
   });
 
+  // Video chat signaling
+  socket.on('offer', ({ offer, to, from, username }) => {
+    io.to(to).emit('offer', { offer, from, username });
+  });
+
+  socket.on('answer', ({ answer, to, username }) => {
+    io.to(to).emit('answer', { answer, from: socket.id, username });
+  });
+
+  socket.on('candidate', ({ candidate, to }) => {
+    io.to(to).emit('candidate', { candidate, from: socket.id });
+  });
+
+  // Screen sharing
+  socket.on('screenShareStarted', ({ room, userId }) => {
+    socket.to(room).emit('screenShareStarted', { userId });
+  });
+
+  socket.on('screenShareEnded', ({ room, userId }) => {
+    socket.to(room).emit('screenShareEnded', { userId });
+  });
+
+  // Raise hand feature
+  socket.on('raiseHand', ({ room, username, isRaised }) => {
+    socket.to(room).emit('handRaised', { username, isRaised });
+  });
+
+  // Reactions
+  socket.on('sendReaction', ({ room, username, reaction }) => {
+    socket.to(room).emit('reaction', { username, reaction });
+  });
+
+  // Chat messages
   socket.on('chatMessage', async ({ room, username, message }) => {
-      const newMessage = await new Message({ room, username, message }).save();
-      io.to(room).emit('chatMessage', newMessage);
+    const newMessage = await new Message({ room, username, message }).save();
+    io.to(room).emit('chatMessage', newMessage);
   });
 
-  socket.on('typing', ({ username, room }) => socket.to(room).emit('userTyping', `${username} is typing...`));
-  socket.on('stopTyping', ({ room }) => socket.to(room).emit('userStoppedTyping'));
+  // Typing indicators
+  socket.on('typing', ({ username, room }) => {
+    socket.to(room).emit('userTyping', `${username} is typing...`);
+  });
 
-  socket.on('leaveRoom', ({ username, room }) => {
-      socket.leave(room);
-      onlineUsers[room] = onlineUsers[room]?.filter(user => user !== username);
+  socket.on('stopTyping', ({ room }) => {
+    socket.to(room).emit('userStoppedTyping');
+  });
+
+  // Leaving room
+  socket.on('leaveRoom', ({ username, room, isVideoRoom = false }) => {
+    if (onlineUsers[room]) {
+      onlineUsers[room] = onlineUsers[room].filter(user => user.userId !== socket.id);
+      
+      if (isVideoRoom) {
+        socket.broadcast.to(room).emit('userLeft', { userId: socket.id, username });
+        io.to(room).emit('participantCount', { count: onlineUsers[room].length });
+      } else {
+        socket.broadcast.to(room).emit('userLeft', `${username} has left the chat`);
+      }
+      
       io.to(room).emit('onlineUsers', onlineUsers[room]);
-      socket.broadcast.to(room).emit('userLeft', `${username} has left the chat`);
+    }
+    socket.leave(room);
   });
-  socket.on('screenShare', ({ room, isSharing }) => {
-    socket.broadcast.to(room).emit('screenShare', { isSharing, userId: socket.id });
-  });
-  socket.on('offer', ({ offer, to }) => io.to(to).emit('offer', { offer, from: socket.id }));
-  socket.on('answer', ({ answer, to }) => io.to(to).emit('answer', { answer, from: socket.id }));
-  socket.on('candidate', ({ candidate, to }) => io.to(to).emit('candidate', { candidate, from: socket.id }));
 
+  // Disconnection
   socket.on('disconnect', () => {
-      console.log('User disconnected:', socket.id);
-      Object.keys(onlineUsers).forEach(room => {
-          onlineUsers[room] = onlineUsers[room].filter(user => user !== socket.id);
-          io.to(room).emit('onlineUsers', onlineUsers[room]);
-      });
-      io.emit('userLeft', socket.id);
+    console.log('User disconnected:', socket.id);
+    Object.keys(onlineUsers).forEach(room => {
+      const userIndex = onlineUsers[room].findIndex(user => user.userId === socket.id);
+      if (userIndex !== -1) {
+        const username = onlineUsers[room][userIndex].username;
+        onlineUsers[room].splice(userIndex, 1);
+        io.to(room).emit('userLeft', { userId: socket.id, username });
+        io.to(room).emit('onlineUsers', onlineUsers[room]);
+        io.to(room).emit('participantCount', { count: onlineUsers[room].length });
+      }
+    });
   });
 });
 
-
-
-
-
-// io.on('connection', (socket) => {
-//   console.log('New user connected:', socket.id);
-
-//   socket.on('joinRoom', async ({ username, room }) => {
-//     socket.join(room);
-
-//     const messages = await Message.find({ room }).sort('createdAt');
-//     socket.emit('chatHistory', messages);
-
-//     socket.broadcast.to(room).emit('userJoined', `${username} has joined the chat`);
-
-//     if (!onlineUsers[room]) {
-//       onlineUsers[room] = [];
-//     }
-//     if (!onlineUsers[room].includes(username)) {
-//       onlineUsers[room].push(username);
-//     }
-
-//     // Emit the updated list of online users to the room
-//     io.to(room).emit('onlineUsers', onlineUsers[room]);
-//   });
-
-//   socket.on('chatMessage', async ({ room, username, message }) => {
-//     const newMessage = new Message({ room, username, message });
-//     await newMessage.save();
-//     io.to(room).emit('chatMessage', newMessage);
-//   });
-
-//   socket.on('typing', ({ username, room }) => {
-//     socket.to(room).emit('userTyping', `${username} is typing...`);
-//   });
-
-//   socket.on('stopTyping', ({ room }) => {
-//     socket.to(room).emit('userStoppedTyping');
-//   });
-
-//   socket.on('leaveRoom', ({ username, room }) => {
-//     socket.leave(room);
-
-//     if (onlineUsers[room]) {
-//       onlineUsers[room] = onlineUsers[room].filter((user) => user !== username);
-//       // Emit the updated list of online users to the room
-//       io.to(room).emit('onlineUsers', onlineUsers[room]);
-//     }
-
-//     socket.broadcast.to(room).emit('userLeft', `${username} has left the chat`);
-//   });
-
-//   socket.on('disconnect', () => {
-//     console.log('User disconnected:', socket.id);
-
-//     for (const room in onlineUsers) {
-//       onlineUsers[room] = onlineUsers[room].filter((user) => user !== socket.id);
-//       io.to(room).emit('onlineUsers', onlineUsers[room]);
-//     }
-//   });
-// });
-
-
-// io.on('connection', (socket) => {
-//   console.log('New user connected:', socket.id);
-
-//   socket.on('joinRoom', ({ username, room }) => {
-//       socket.join(room);
-//       io.to(room).emit('onlineUsers', `${username} joined`);
-
-//       // Notify existing users to start WebRTC signaling
-//       socket.broadcast.to(room).emit('userJoined', { id: socket.id, username });
-//   });
-//   socket.on('screenShare', ({ room, isSharing }) => {
-//     socket.broadcast.to(room).emit('screenShare', { isSharing, userId: socket.id });
-//   });
-
-//   socket.on('offer', ({ offer, to }) => {
-//       io.to(to).emit('offer', { offer, from: socket.id });
-//   });
-
-//   socket.on('answer', ({ answer, to }) => {
-//       io.to(to).emit('answer', { answer, from: socket.id });
-//   });
-
-//   socket.on('candidate', ({ candidate, to }) => {
-//       io.to(to).emit('candidate', { candidate, from: socket.id });
-//   });
-
-//   socket.on('disconnect', () => {
-//       console.log('User disconnected:', socket.id);
-//       io.emit('userLeft', socket.id);
-//   });
-// });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
